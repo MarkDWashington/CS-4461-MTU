02d4734f12162f78d78b5f02bd2d6c46e4266d82
Mark Washington <markdw88@gmail.com>
2022-02-04 10:17:44 -0500

    Updates

--- PA1/QOTDClient.py
+++ PA1/QOTDClient.py
@@ -1,11 +1,20 @@
 import socket, sys
 
+# Make sure we specify an argument
 if len(sys.argv) < 2:
     exit()
 
+# Load IP from args
 ip = sys.argv[1]
 
+# Create a UDP socket 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+input("Press enter to receive a random quote!")
+# Send a packet using the IP specified, port 9876
 s.sendto("qotd".encode(), (ip, 9876))
+# Wait for a response from the server
 data, addr = s.recvfrom(1024)
-print(data)
+# Print the data we received
+data = data.decode("utf-8")
+print(f"Random quote from server: \"{data}\"")
--- PA1/QOTDServer.py
+++ PA1/QOTDServer.py
@@ -1,20 +1,40 @@
 import socket, json, random
 
+# FIXME: Add more quotes
+
+print("Server is starting")
+
+# Load quotes from json file and parse to dict
 quotes = {}
 with open("quotes.json", "r") as f: quotes = json.load(f)
 
+port = 9876
+
+# Create a UDP socket on localhost, port 9876
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("127.0.0.1", 9876))
 
+# Start main loop
+print(f"Waiting for connection on port {port}")
+
 while True:
     try:
+        print("Server is listening...")
+        # Receive four bytes worth of data
         data, addr = s.recvfrom(4)   
+
+        print("Client is connected")
+
+        # Check if data is the "qotd" keyword
         if data == b"qotd":
+            print("Client request a quote")
+
+            # Pick a random quote and send it back to the address we received from
             q: str = quotes["quotes"][random.randint(0, len(quotes["quotes"]) - 1)]
+            print(f"Sending random quote: \"{q}\"")
             s.sendto(q.encode(), addr)
+
     except KeyboardInterrupt:
+        # Handle keyboard interrupts for clean exits
         print("\nUser stopped server")
-        break
-    except Exception:
-        print("Something went wrong :(")
         break
